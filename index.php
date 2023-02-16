5036a86297f4e5ef34891ef43caab561cbc1077e
DDeganoFrontaly <ddegano@frontaly.com>
2023-02-16 03:21:58 -0300

    Sass

--- index.php
+++ index.php
@@ -4,19 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario de Inscripcion</title>
+    <title>Formulario de Registro</title>
     <link rel="stylesheet" href="./resources/styles.css">
 </head>
 <body class="inscripcion">
-    <form method="post">
-        <h1>Formulario de Inscripcion</h1>
-        <input type="text" name="nombre" placeholder="Nombre y Apellido">
-        <input type="text" name="edad" placeholder="Edad">
-        <input type="text" name="dni" placeholder="Dni">
-        <input type="email" name="email" placeholder="Correo electronico">
-        <input type="text" name="telefono" placeholder="Telefono">
-        <input type="submit" name="register">
-    </form>
+    <section class="inscripcion_form_wrp">
+        <form class="inscripcion_form" method="post">
+            <h1>Formulario de Registro</h1>
+            <input type="text" name="nombre" placeholder="Nombre y Apellido">
+            <input type="text" name="edad" placeholder="Edad">
+            <input type="text" name="dni" placeholder="Dni">
+            <input type="email" name="email" placeholder="Correo electronico">
+            <input type="text" name="telefono" placeholder="Telefono">
+            <input class="inscripcion_form_btn" type="submit" name="register">
+        </form>
+    </section>
     <?php
     include("registrar.php");
     ?>
